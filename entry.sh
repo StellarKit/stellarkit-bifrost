177f8bdc966563961301cf3c4180a520c57567e9
Steve Gehrman <sgehrman@gmail.com>
2017-12-29 15:18:58 -0800

    sf

--- entry.sh
+++ entry.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+DB_URL=$BIFROST_DB_DSN DB_DUMP_FILE=/go/src/github.com/stellar/go/services/bifrost/database/migrations/01_init.sql /go/bin/initbifrost
+
 build-config /config.toml > /opt/bifrost/bifrost.cfg
 
 /go/bin/bifrost server -c /opt/bifrost/bifrost.cfg
---
+++ initbifrost/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"io/ioutil"
+	"strings"
+	_ "github.com/lib/pq"
+	"fmt"
+	"os"
+)
+
+func main()  {
+	dbUrl := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Cannot parse db-url: `%s`", dbUrl))
+	}
+
+	dumpFile := os.Getenv("DB_DUMP_FILE")
+	file, err := ioutil.ReadFile(dumpFile)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Cannot open sql-dump: `%s`", dumpFile))
+	}
+
+	requests := strings.Split(string(file), ";")
+
+	// TODO: put it in transaction
+	for _, request := range requests {
+		_, err := db.Exec(request)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	ethereumLastBlock := os.Getenv("ETHEREUM_LAST_BLOCK")
+	if ethereumLastBlock != "" {
+		_, err := db.Exec("UPDATE key_value_store SET value = $1 WHERE key = $2", ethereumLastBlock, "ethereum_last_block")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	bitcoinLastBlock := os.Getenv("BITCOIN_LAST_BLOCK")
+	if bitcoinLastBlock != "" {
+		_, err := db.Exec("UPDATE key_value_store SET value = $1 WHERE key = $2", bitcoinLastBlock, "bitcoin_last_block")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
