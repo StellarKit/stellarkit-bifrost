a7fa08cc728420526e9ad7bba4bb4c5fc930810a
Steve Gehrman <sgehrman@gmail.com>
2017-12-29 21:04:31 -0800

    fg

--- entry.sh
+++ entry.sh
@@ -2,8 +2,37 @@
 
 set -e
 
-# DB_URL=$BIFROST_DB_DSN DB_DUMP_FILE=/go/src/github.com/stellar/go/services/bifrost/database/migrations/01_init.sql /go/bin/initbifrost
+function main() {
+  while ! psql -U bifrost -c 'select 1' bifrostdb &> /dev/null ; do
+    echo "Waiting for bifrost postgres to be available..."
+    sleep 1
+  done
 
-build-config /config.toml > /opt/bifrost/bifrost.cfg
+  init_bifrost_db
+  start_bifrost
+}
 
-/go/bin/bifrost server -c /opt/bifrost/bifrost.cfg
+function init_bifrost_db() {
+	if [ -f /opt/bifrost/.bifrostdb-initialized ]; then
+		echo "Bifrost DB: already initialized"
+		return 0
+	fi
+
+  echo "Bifrost DB: Initializing"
+  DB_URL=$BIFROST_DB_DSN
+  DB_DUMP_FILE=/go/src/github.com/stellar/go/services/bifrost/database/migrations/01_init.sql
+  /go/bin/initbifrost
+
+  # not sure if needed, is this async?
+  sleep 1
+
+	touch /opt/bifrost/.bifrostdb-initialized
+ }
+
+function start_bifrost() {
+  build-config /config.toml > /opt/bifrost/bifrost.cfg
+
+  /go/bin/bifrost server -c /opt/bifrost/bifrost.cfg
+}
+
+main
