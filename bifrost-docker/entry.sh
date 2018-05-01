536124760ebb5438a6e3b6db50ffc6f59ed780a7
Steve Gehrman <sgehrman@gmail.com>
2018-05-01 01:07:31 -0700

    fixes for .2

--- bifrost-docker/entry.sh
+++ bifrost-docker/entry.sh
@@ -14,21 +14,7 @@ function main() {
     sleep 1
   done
 
-  init_bifrost_db
   start_bifrost
-}
-
-function init_bifrost_db() {
-  if [ -f /opt/bifrost/.bifrostdb-initialized ]
-  then
-    echo "Bifrost DB: already initialized"
-    return 0
-  fi
-
-  echo "Bifrost DB: Initializing"
-  DB_URL=$BIFROST_DB_DSN DB_DUMP_FILE=/go/src/github.com/stellar/go/services/bifrost/database/migrations/01_init.sql /go/bin/initbifrost
-
-  touch /opt/bifrost/.bifrostdb-initialized
 }
 
 function start_bifrost() {
